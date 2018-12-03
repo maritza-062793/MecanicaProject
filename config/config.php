c71d1e3b9f8290673496a6e250781d34e414aa4a
Maritza <rmpaspuezana@utn.edu.ec>
2018-12-03 10:12:45 -0500

    actua

--- config/config.php
+++ config/config.php
@@ -1,9 +1,14 @@
 <?php
-
+/*
 	/*Datos de conexion a la base de datos*/
-	define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
-	define('DB_USER', 'root');//Usuario de tu base de datos
-	define('DB_PASS', '');//Contraseña del usuario de la base de datos
+//	define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
+//	define('DB_USER', 'root');//Usuario de tu base de datos
+//	define('DB_PASS', '');//Contraseña del usuario de la base de datos
+//	define('DB_NAME', 'taller');//Nombre de la base de datos
+
+	define('DB_HOST', 'MySQL 8.0 Server:3306');//DB_HOST:  generalmente suele ser "127.0.0.1"
+	define('DB_USER', 'mari_2793');//Usuario de tu base de datos
+	define('DB_PASS', 'nicole_2014');//Contraseña del usuario de la base de datos
 	define('DB_NAME', 'taller');//Nombre de la base de datos
 
 	$con=@mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
